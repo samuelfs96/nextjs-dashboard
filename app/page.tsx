f5bf0cc5e16769e2dccf52e4e79c2692d7bdeee9
samuel <samuelfs96@gmail.com>
2025-08-29 10:32:13 -0300

    pages and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 import Link from 'next/link';
 
